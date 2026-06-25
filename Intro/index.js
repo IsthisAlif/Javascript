02195d39f67a296eaf33057f775f626ecd0537af
IsthisAlif <abdulalif2003@gmail.com>
2026-06-25 15:17:52 +0800

    commit 1

---
+++ Intro/index.js
@@ -0,0 +1,16 @@
+console.log(`Hello`);
+console.log(`I like pizza`);
+
+// window.alert(`This is an alert`);
+// window.alert(`I like pizza`);
+
+document.getElementById("myh1").textContent = `Hello`;
+document.getElementById("myp").textContent = `I like pizza!`
+
+// This is a comment
+
+/* 
+
+This is a comment
+
+*/
