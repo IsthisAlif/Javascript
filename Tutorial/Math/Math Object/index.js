706d7b74aea44dc7256ef2d39511960013a70e2a
IsthisAlif <abdulalif2003@gmail.com>
2026-06-25 20:28:22 +0800

    commit 5

---
+++ Tutorial/Math/Math Object/index.js
@@ -0,0 +1,23 @@
+// Math = built-in object that provides a 
+//        Collection of properties and method
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+//z = Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(x, y);
+//z = Math.sqrt(x);
+//z = Math.log(10);
+//z = Math.sin(45);
+//z = Math.cos(45);
+//z = Math.tan(45);
+//z = Math.abs(x);
+//z = Math.sign(x);
+//let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(min);
