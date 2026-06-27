015f01e80fc21d8cf06774b0eb25d89738946a58
IsthisAlif <abdulalif2003@gmail.com>
2026-06-27 16:36:40 +0800

    commit 12

---
+++ Tutorial/intermediate/Logical Operators/index.js
@@ -0,0 +1,24 @@
+// logical operators = used to combine or manipulate boolean values
+//                     (true of false)
+
+//                     AND = &&
+//                     OR  = ||
+//                     NOT = !
+
+const temp = 30;
+
+if(temp > 0 && temp <= 30){
+    console.log("The weather is good");
+}
+else{
+    console.log("The weather is bad");
+}
+
+const isSunny = true;
+
+if(!isSunny){
+    console.log("It is sunny");
+}
+else{
+    console.log("It is cloudy");
+}
