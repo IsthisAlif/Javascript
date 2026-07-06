89493c5a726ca9a78e20ab56a4d5471b22311958
IsthisAlif <abdulalif2003@gmail.com>
2026-07-06 19:29:58 +0800

    commit 29

---
+++ Tutorial/Objects/Classes/index.js
@@ -0,0 +1,37 @@
+// class = (ES6 feature) provides a more structured and cleaner way to
+//         work with objects compared to traditional constructor functions
+//         ex. static keyword, encapsulation, inheritance
+
+class Product{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+
+    displayProduct(){
+        console.log(`Product: ${this.name}`);
+        console.log(`Price: $${this.price.toFixed(2)}`);
+    }
+
+    calculateTotal(saleTax){
+        return this.price + (this.price * saleTax);
+    }
+}
+
+const salesTax = 0.05;
+
+const product1 = new Product("Shirt", 19.99);
+const product2 = new Product("Pants", 22.50);
+const product3 = new Product("Underwear", 100.00);
+
+product1.displayProduct();
+product2.displayProduct();
+product3.displayProduct();
+
+const total1 = product1.calculateTotal(salesTax);
+const total2 = product2.calculateTotal(salesTax);
+const total3 = product3.calculateTotal(salesTax);
+
+console.log(`Total price (with tax): $${total1.toFixed(2)}`);
+console.log(`Total price (with tax): $${total2.toFixed(2)}`);
+console.log(`Total price (with tax): $${total3.toFixed(2)}`);
