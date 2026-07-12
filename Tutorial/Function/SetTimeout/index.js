a76f1fae4b6cfa7039005751dfbebb3abdfb3d4f
IsthisAlif <abdulalif2003@gmail.com>
2026-07-12 21:57:44 +0800

    commit 41

---
+++ Tutorial/Function/SetTimeout/index.js
@@ -0,0 +1,19 @@
+// setTimeout() = a function in Javascript that allows you to schedule
+//                the execution of a function after an amount of time (milliseconds)
+//                Times are approximate (varies based on the workload of the Javascript runtime env.)
+
+//                setTimeout(callback, delay);
+//                clearTimeout(timeoutId);
+
+let timeoutId;
+
+function startTimer(){
+    timeoutId = setTimeout(() => window.alert("Hello"), 3000);
+    console.log("STARTED");
+}
+
+function clearTimer(){
+    clearTimeout(timeoutId);
+    console.log("CLEARED");
+}
+
