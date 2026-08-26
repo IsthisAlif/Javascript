67e00594c710149a870c1a2f4d20ac711204fae3
IsthisAlif <abdulalif2003@gmail.com>
2026-08-26 23:27:35 +0800

    commit 52

---
+++ Tutorial/Events/Key Event/index.js
@@ -0,0 +1,35 @@
+// eventListener = Listen for specific events to create interactive web pages
+//                 events: keydown, keyup
+//                 document.addEventListener(event, callback);
+
+const mybox = document.getElementById("mybox");
+const moveAmount = 10;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", event => {
+
+    if(event.key.startsWith("Arrow")){
+
+        event.preventDefault();
+
+        switch(event.key){
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+            case "ArrowDown":
+                y += moveAmount;
+                break;
+            case "ArrowLeft":
+                x -= moveAmount;
+                break;
+            case "ArrowRight":
+                x += moveAmount;
+                break;
+        }
+
+        mybox.style.top = `${y}px`;
+        mybox.style.left = `${x}px`;
+        
+    }
+});
